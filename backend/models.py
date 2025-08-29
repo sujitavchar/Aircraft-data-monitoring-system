df599ed8c93d1809b61d091853aa2d44c050e36a
Sujit Avchar <145844481+sujitavchar@users.noreply.github.com>
2025-08-29 17:58:53 +0530

    Created pydantic models

---
+++ backend/models.py
@@ -0,0 +1,25 @@
+from  pydantic import BaseModel
+from typing import List, Any
+
+# Request structure for  flight data row
+class DataRow(BaseModel):
+    timestamp: str
+    altitude_ft : float
+    airspeed_kts: float
+    pitch_deg: float
+    roll_deg: float
+    vertical_speed_fpm: float
+    engine1_temp_C: float
+    engine2_temp_C : float
+    heading_deg: float
+    flaps_deg: float
+    spoiler_percent: float
+    phase: str
+
+# request structure for anomaly 
+class Anomaly(BaseModel):
+    timestamp: str
+    parameter: str
+    value: Any
+    rule: str
+    phase: str
